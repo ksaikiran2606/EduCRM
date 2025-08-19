8b753515260ca16ef62c61988ffb89d9106eddc9
k.sai kiran <ksaikiranchary2606@gmail.com>
2025-08-19 17:58:16 +0530

    empty

--- School/student/contact.py
+++
@@ -1,4 +0,0 @@
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = '__all__'  # This will include 'contact'
--- School/trainers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.TrainerList, name="alltrainers"),
-    path('addtrainer/', views.addTrainers, name="addtrainer"),
-    path('update_trainer/<str:id>', views.update_trainer, name="update"),
-    path("delete/<str:id>", views.delete_trainer, name='delete'),
-]
